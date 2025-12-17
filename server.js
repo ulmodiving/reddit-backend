d726c60daacff0254f325a6629add1960d0bfa23
ulmodiving <155707961+ulmodiving@users.noreply.github.com>
2025-12-17 19:48:21 +0100

    server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const { exec } = require("child_process");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
@@ -26,7 +27,8 @@ app.post("/download", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
-});
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
